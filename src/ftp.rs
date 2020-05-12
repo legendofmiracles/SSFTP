984f8165ad915e4a99c98e45931949b967768252
russon@awty.org <russon@awty.org>
2020-05-12 13:37:27 -0500

    added ftp support

--- src/ftp.rs
+++ src/ftp.rs
@@ -1,4 +1,23 @@
-pub fn init(){
-    // initialize
-    println!("Beginning initiation of FTP client...")
+use std::path::Path;
+mod parseFtp;
+
+pub fn checkIfFileExists(file: String) -> bool {
+    let exists = Path::new(file).exists();
+    if exists == true {
+        println!("{0} exists on machine.", file);
+        send(file); // send the file.
+    } else {
+        println!("{0} doesn't exist. Please check your file name and try again", file);
+    }
+    Path::new(file).exists()
+}
+
+pub fn send(file: String){
+    if checkIfFileExists(file){
+        println!("Sending {0}", file);
+    }
+}
+
+pub fn init(file: String){
+
 }
--- src/sftp.rs
+++ src/sftp.rs
@@ -2,6 +2,13 @@ use std::path::Path;
 mod parseSftp;
 
 pub fn checkIfFileExists(file: String) -> bool {
+    let exists = Path::new(file).exists();
+    if exists == true {
+        println!("{0} exists on machine.", file);
+        send(file); // send the file.
+    } else {
+        println!("{0} doesn't exist. Please check your file name and try again", file);
+    }
     Path::new(file).exists()
 }
 
@@ -11,6 +18,6 @@ pub fn send(file: String){
     }
 }
 
-pub fn init(){
+pub fn init(file: String){
 
 }
