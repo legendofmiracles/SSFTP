8438ec118c55071fd50d555b0473f775e4b7f36e
russon@awty.org <russon@awty.org>
2020-05-15 08:19:14 -0500

    I have no idea. alot of fixes

--- src/server/ftp.rs
+++ src/server/ftp.rs
@@ -4,9 +4,14 @@
 
 
 // imports
-
+mod parseFtp;
 
 // public functions (other then init)
+pub fn sendFile(){
+    // we need to parse the file, so we are going to check for malware
+    parseFtp::checkForMal("eatmynuggets.bat".to_string()); // should return true
+    parseFtp::checkForMal("latexdocumentation.txt".to_string()); // should return false
+}
 
 
 pub fn startServer(){
@@ -17,4 +22,5 @@ pub fn startServer(){
 // init function
 pub fn init(){
     println!("Initializing SSFTP.");
+    sendFile();
 }
---
+++ src/server/parseFtp.rs
@@ -0,0 +1,22 @@
+// parse the files that are sent through FTP.
+
+
+// imports
+
+
+// global scoped, immutable variables
+
+
+// global scoped, mutable variables
+
+// public functions
+
+// check for malware checks if the extension is malicious
+// example:
+// checkForMal(".exe"); true
+// checkForMal(".pdf"); false
+pub fn checkForMal(file: std::string::String) {
+    //let mut splitFile = file.split(".");
+    print!("{}", file);
+
+}
