c781df86c0cccf661a9be28023057c8a747747f8
AVCADO <devavcado@gmail.com>
2020-05-27 16:18:01 -0500

    Alot of changes to the server

--- src/server/extScripts/server/index.py
+++ src/server/extScripts/server/index.py
@@ -1,3 +1,4 @@
+import sys
 import flask
 from flask import Flask, render_template
 
@@ -8,5 +9,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/client')
+def client():
+    return render_template("client.html", operatingClientSystem=sys.platform, operatingServerSystem="Ubuntu 18.04 LTS")
 
 app.run(debug=True)
