ae3ceb36f522da91a2a3d0945ec935ae95b702c8
AVCADO <devavcado@gmail.com>
2020-06-01 11:42:35 +0000

    added stuff

--- src/server/ftp.rs
+++ src/server/ftp.rs
@@ -8,6 +8,9 @@ mod parseFtp;
 pub fn startServer(){
     println!("Work in progress FTP server init.");
     parseFtp::checkForMal("etc.sh".to_owned());
+    println!("TESTING .TXT DOWNLOADING!");
+    parseFtp::checkForMal("test.txt".to_owned());
+    parseFtp::recursivelyDownloadFile("$HOME/Desktop/SSFTP/src/server/test.txt".to_owned());
 }
 
 
